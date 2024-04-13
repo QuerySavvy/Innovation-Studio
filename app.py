9578ebdcb48b5ef8023e4ac9b982f3147d4689a5
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-04-13 21:52:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,10 +27,6 @@ st.write(location)
 lat = location[0]
 lng = location[1]
 
-# Sidebar message
-st.sidebar.write("Hello world")
-
-
 
 st.header("Geolocation Section")
 # Reverse geocoding to get user's address
@@ -44,30 +40,53 @@ st.write(f"**Your address is:** {location.address}")
 my_string = location.address
 my_list = my_string.split(",")
 
-country = my_list[-1]
-postcode = my_list[-2]
+try:
+    country = my_list[-1]
+except IndexError:
+    country = None
 
-if exist mylist[-3]:
+try:
+    postcode = my_list[-2]
+except IndexError:
+    postcode = None
+
+try:
     other1 = my_list[-3]
-if exists(my_list[-4]):
+except IndexError:
+    other1 = None
+
+try:
     other2 = my_list[-4]
-if exists(my_list[-5]):
+except IndexError:
+    other2 = None
+
+try:
     other3 = my_list[-5]
-if exists(my_list[-6]):
+except IndexError:
+    other3 = None
+
+try:
     other4 = my_list[-6]
-if exists(my_list[-7]):
+except IndexError:
+    other4 = None
+
+try:
     other5 = my_list[-7]
-if exists(my_list[-8]):
+except IndexError:
+    other5 = None
+
+try:
     other6 = my_list[-8]
+except IndexError:
+    other6 = None
+
 
 # Displaying address components
 st.write(f"**Country:** {country}")
 st.write(f"**Postcode:** {postcode}")
 st.write(f"**Other1:** {other1}")
-st.write(f"**Other2:** {other2}")
+st.write(f"**State ?:** {other2}")
 st.write(f"**Other3:** {other3}")
-st.write(f"**Other4:** {other4}")
+st.write(f"**Town ?:** {other4}")
 st.write(f"**Other5:** {other5}")
 st.write(f"**Other6:** {other6}")
-
-
