3994fff392074f429ddb960da7e294b5a88af798
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-04-16 22:40:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,94 +1,54 @@
 import streamlit as st
 from geopy.geocoders import Nominatim
-import geocoder
 import cv2
 import numpy as np
 import streamlit as st
+import pandas as pd
+import folium
+from streamlit_folium import st_folium
 
 # Title of the application
 st.title("The desktop prototype")
 
 # File upload section
-
 st.header("Image upload section")
 uploaded_image = st.file_uploader("", accept_multiple_files=True)
 for uploaded_image in uploaded_image:
     image = uploaded_image.read()
     st.image(image, width=250, clamp = True)
 
-# Getting user's location using IP address
-g = geocoder.ip('me')
-location = g.latlng
 
-# Displaying user's location
-#st.write(location)
-
-# Extracting latitude and longitude
-lat = location[0]
-lng = location[1]
 
 
 st.header("Geolocation Section")
-# Reverse geocoding to get user's address
-geolocator = Nominatim(user_agent="rubbish-app")
-location = geolocator.reverse((lat, lng))
 
-# Displaying user's address
-st.write(f"**Your address is:** {location.address}")
-
-# Splitting address into components
-my_string = location.address
-my_list = my_string.split(",")
-
-try:
-    country = my_list[-1]
-except IndexError:
-    country = None
-
-try:
-    postcode = my_list[-2]
-except IndexError:
-    postcode = None
-
-try:
-    other1 = my_list[-3]
-except IndexError:
-    other1 = None
-
-try:
-    other2 = my_list[-4]
-except IndexError:
-    other2 = None
-
-try:
-    other3 = my_list[-5]
-except IndexError:
-    other3 = None
-
-try:
-    other4 = my_list[-6]
-except IndexError:
-    other4 = None
-
-try:
-    other5 = my_list[-7]
-except IndexError:
-    other5 = None
-
-try:
-    other6 = my_list[-8]
-except IndexError:
-    other6 = None
+file = "https://raw.githubusercontent.com/matthewproctor/australianpostcodes/master/australian_postcodes.csv"
 
 
-# Displaying address components
-st.write(f"**Country:** {country}")
-st.write(f"**Postcode:** {postcode}")
-st.write(f"**Other1:** {other1}")
-st.write(f"**Other2:** {other2}")
-st.write(f"**Other3:** {other3}")
-st.write(f"**Other4:** {other4}")
-st.write(f"**Other5:** {other5}")
-st.write(f"**Other6:** {other6}")
+
+data = pd.read_csv(file, usecols=[2,3,14,15,35])
+data = data[data['state'] =="NSW"]
+data = data.drop_duplicates('locality')
 
 
+suburbs = data['locality'].to_list()
+suburbs.sort()
+
+selected_suburb = st.selectbox("Suburb select", suburbs, index=None, placeholder="Select a Suburb ...",)
+if selected_suburb is None:
+    st.stop()
+
+
+latitude = data["Lat_precise"][data["locality"]==selected_suburb].values[0]
+longitude = data["Long_precise"][data["locality"]==selected_suburb].values[0]
+st.write(latitude)
+st.write(longitude)
+
+
+coordinates = (latitude,longitude)
+
+map = folium.Map(location=coordinates, zoom_start=15)
+
+st.header("Geo Map")
+
+st_folium(map,width = 700)
