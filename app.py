98fb80a123e4ee2aca301f258ef7334e7edc3286
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-04-17 16:34:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,77 @@
 import streamlit as st
-from geopy.geocoders import Nominatim
-import cv2
-import numpy as np
-import streamlit as st
 import pandas as pd
 import folium
 from streamlit_folium import st_folium
+from geopy.geocoders import Nominatim
 
 # Title of the application
-st.title("The desktop prototype")
+st.title("The Desktop Prototype")
 
 # File upload section
-st.header("Image upload section")
-uploaded_image = st.file_uploader("", accept_multiple_files=True)
-for uploaded_image in uploaded_image:
+st.header("Image Upload Section")
+uploaded_images = st.file_uploader("Upload Image(s)", accept_multiple_files=True)
+for uploaded_image in uploaded_images:
     image = uploaded_image.read()
-    st.image(image, width=250, clamp = True)
+    st.image(image, width=250, clamp=True)
 
-
-
-
+# Geolocation Section
 st.header("Geolocation Section")
 
+# Load suburb data
 file = "https://raw.githubusercontent.com/matthewproctor/australianpostcodes/master/australian_postcodes.csv"
-
-
-
-data = pd.read_csv(file, usecols=[2,3,14,15,35])
-data = data[data['state'] =="NSW"]
+data = pd.read_csv(file, usecols=[2, 3, 14, 15, 35])
+data = data[data['state'] == "NSW"]
 data = data.drop_duplicates('locality')
 
-
+# Select suburb
 suburbs = data['locality'].to_list()
 suburbs.sort()
-
-selected_suburb = st.selectbox("Suburb select", suburbs, index=None, placeholder="Select a Suburb ...",)
+selected_suburb = st.selectbox("Suburb", suburbs, index=None, placeholder="Select a Suburb ...",)
 if selected_suburb is None:
     st.stop()
 
-
-latitude = data["Lat_precise"][data["locality"]==selected_suburb].values[0]
-longitude = data["Long_precise"][data["locality"]==selected_suburb].values[0]
-st.write(latitude)
-st.write(longitude)
+# Get latitude and longitude of selected suburb
+latitude = data.loc[data['locality'] == selected_suburb, "Lat_precise"].values[0]
+longitude = data.loc[data['locality'] == selected_suburb, "Long_precise"].values[0]
 
 
-coordinates = (latitude,longitude)
 
-map = folium.Map(location=coordinates, zoom_start=15)
+selected_street = st.text_input("Street Name")
+selected_number = st.text_input("Street Number")
 
-st.header("Geo Map")
 
-st_folium(map,width = 700)
+# Get location info using geopy
+geolocator = Nominatim(user_agent="UTS_APP")
+
+if selected_street is not None and selected_street != "":
+
+    if selected_number is not None and selected_number != "":
+        geo_location = geolocator.geocode(selected_number +" "+selected_street +" "+ selected_suburb+" "+"NSW, Australia")
+        coordinates = (geo_location.latitude, geo_location.longitude)
+        map = folium.Map(location=coordinates, zoom_start=17)
+        folium.Marker([geo_location.latitude, geo_location.longitude]).add_to(map)
+
+    else:
+        geo_location = geolocator.geocode(selected_street +" "+ selected_suburb+" "+"NSW, Australia")
+        coordinates = (geo_location.latitude, geo_location.longitude)
+        map = folium.Map(location=coordinates, zoom_start=17)
+        folium.Marker([geo_location.latitude, geo_location.longitude]).add_to(map)
+
+else:
+    geo_location = geolocator.geocode(selected_suburb+" "+", NSW, Australia")
+    coordinates = (geo_location.latitude, geo_location.longitude)
+    map = folium.Map(location=coordinates, zoom_start=14)
+
+# Display location address and coordinates
+address = geo_location.address
+
+if not address[0].isdigit() and selected_number!= "":
+    address = selected_number + ", " + address
+    st.write("TESTING FEATURE: not a digit")
+
+st.write("Address:", address)
+st.write("Coordinates:", (geo_location.latitude, geo_location.longitude))
+
+# Display map
+st_folium(map, height=400)
+
