a381beac72226a3ae56ad25289aeb9039f5a3945
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-04-25 15:56:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,70 +1,111 @@
 import streamlit as st
+from inference_sdk import InferenceHTTPClient
+from PIL import Image, ImageOps
 import pandas as pd
 import folium
 from streamlit_folium import st_folium
 from geopy.geocoders import Nominatim
 
-# Title of the application
-st.title("The Desktop Prototype")
+# initialize the client
+CLIENT = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key="uFsY2MHzUdNWrdNGsjcl"
+)
 
-# File upload section
-st.header("Image Upload Section")
-uploaded_images = st.file_uploader("Upload Image(s)", accept_multiple_files=True)
-for uploaded_image in uploaded_images:
-    image = uploaded_image.read()
-    st.image(image, width=250, clamp=True)
+# Function to perform inference on uploaded image
+def rubbish_detector(image_file):
+    with st.spinner('Performing image classification...'):
+        # Perform inference
+        result = CLIENT.infer(image_file, model_id="rubpred/4")
 
-# Geolocation Section
-st.header("Geolocation Section")
+    # Extract the item and confidence
+    detected_object = result["top"]
+    confidence = format(result["confidence"],".2%")
+    return detected_object, confidence
 
+@st.cache_data
+def loadlocationdata():
 # Load suburb data
-file = "https://raw.githubusercontent.com/matthewproctor/australianpostcodes/master/australian_postcodes.csv"
-data = pd.read_csv(file, usecols=[2, 3, 14, 15, 35])
-data = data[data['state'] == "NSW"]
-data = data.drop_duplicates('locality')
+    file = "https://raw.githubusercontent.com/matthewproctor/australianpostcodes/master/australian_postcodes.csv"
+    data = pd.read_csv(file, usecols=[2, 3, 14, 15, 35])
+    data = data[data['state'] == "NSW"]
+    data = data.drop_duplicates('locality')
+    suburbs = data['locality'].to_list()
+    suburbs.sort()
+    return suburbs
 
-# Select suburb
-suburbs = data['locality'].to_list()
-suburbs.sort()
-selected_suburb = st.selectbox("Suburb", suburbs, index=None, placeholder="Select a Suburb ...",)
-if selected_suburb is None:
-    st.stop()
+def geolocate():
+    # Get location info using geopy
+    geolocator = Nominatim(user_agent="UTS_APP")
+    if selected_street is not None and selected_street != "":
+        if selected_number is not None and selected_number != "":
+            geo_location = geolocator.geocode(selected_number +" "+selected_street +" "+ selected_suburb+" "+"NSW, Australia")
+            coordinates = (geo_location.latitude, geo_location.longitude)
+            map = folium.Map(location=coordinates, zoom_start=17)
+            folium.Marker([geo_location.latitude, geo_location.longitude]).add_to(map)
+        else:
+            geo_location = geolocator.geocode(selected_street +" "+ selected_suburb+" "+"NSW, Australia")
+            coordinates = (geo_location.latitude, geo_location.longitude)
+            map = folium.Map(location=coordinates, zoom_start=17)
+            folium.Marker([geo_location.latitude, geo_location.longitude]).add_to(map)
+    else:
+        geo_location = geolocator.geocode(selected_suburb+" "+", NSW, Australia")
+        coordinates = (geo_location.latitude, geo_location.longitude)
+        map = folium.Map(location=coordinates, zoom_start=14)
+    # Display location address and coordinates
+    address = geo_location.address
+    if not address[0].isdigit() and selected_number!= "":
+        address = selected_number + ", " + address
+        st.warning('Unable to find exact location on map', icon="⚠️")
+    st.write("Address:", address)
+    #st.write("Coordinates:", (geo_location.latitude, geo_location.longitude))
+    # Display map
+    return st_folium(map, height=400)
 
-# Get latitude and longitude of selected suburb
-latitude = data.loc[data['locality'] == selected_suburb, "Lat_precise"].values[0]
-longitude = data.loc[data['locality'] == selected_suburb, "Long_precise"].values[0]
 
-selected_street = st.text_input("Street Name")
-selected_number = st.text_input("Street Number")
+# ----------------     Streamlit app     ----------------
+st.title("The Desktop Prototype")
+st.header("Image Upload Section")
 
-# Get location info using geopy
-geolocator = Nominatim(user_agent="UTS_APP")
+# Allow user to upload an image
+uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+if uploaded_image is not None:
+    # Display the uploaded image
+    image = Image.open(uploaded_image)
+    st.image(image, width=250, caption="Uploaded Image")
+
+# Define a SessionState object
+session_state = st.session_state
+
+# Perform inference if an image is uploaded and the function has not been run yet
+if uploaded_image is not None and 'detected_object' not in session_state:
+    # Run the rubbish_detector function
+    detected_object, confidence = rubbish_detector(image)
+    session_state['detected_object'] = detected_object
+    session_state['confidence'] = confidence
+
+# Load location data
+suburbs = loadlocationdata()
+
+# Allow user to select their location
+selected_suburb = st.selectbox("Suburb", suburbs, index=None, placeholder="Select a Suburb . . .",)
+col1, col2 = st.columns(2)
+selected_street = col1.text_input("Street Name", placeholder="Enter a Street Name . . .   e.g. Smith Street")
+selected_number = col2.text_input("Street Number", placeholder="Optional")
 
 if selected_street is not None and selected_street != "":
-    if selected_number is not None and selected_number != "":
-        geo_location = geolocator.geocode(selected_number +" "+selected_street +" "+ selected_suburb+" "+"NSW, Australia")
-        coordinates = (geo_location.latitude, geo_location.longitude)
-        map = folium.Map(location=coordinates, zoom_start=17)
-        folium.Marker([geo_location.latitude, geo_location.longitude]).add_to(map)
-    else:
-        geo_location = geolocator.geocode(selected_street +" "+ selected_suburb+" "+"NSW, Australia")
-        coordinates = (geo_location.latitude, geo_location.longitude)
-        map = folium.Map(location=coordinates, zoom_start=17)
-        folium.Marker([geo_location.latitude, geo_location.longitude]).add_to(map)
-else:
-    geo_location = geolocator.geocode(selected_suburb+" "+", NSW, Australia")
-    coordinates = (geo_location.latitude, geo_location.longitude)
-    map = folium.Map(location=coordinates, zoom_start=14)
+    geolocate()
 
-# Display location address and coordinates
-address = geo_location.address
-
-if not address[0].isdigit() and selected_number!= "":
-    address = selected_number + ", " + address
-    st.warning('Unable to find exact location on map', icon="⚠️")
-
-st.write("Address:", address)
-st.write("Coordinates:", (geo_location.latitude, geo_location.longitude))
-
-# Display map
-st_folium(map, height=400)
+# Display inference results if available
+if 'detected_object' in session_state:
+    with st.container(border=True):
+        string = f"Image matched with {session_state['confidence']} confidence."
+        st.info(string, icon="ℹ️")
+        st.write("The photo submitted looks like a ", session_state['detected_object'], " is that right?")
+        col_1, col_2 = st.columns([.1,1])
+        button_yes = col_1.button("Yes")
+        button_no = col_2.button("No")
+        if button_yes:
+            st.balloons()
+        if button_no:
+            st.snow()
