3b1627c3d9c3947d5977741e75bc1fa818bdbd63
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-13 23:09:10 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -13,7 +13,11 @@ CLIENT = InferenceHTTPClient(
 )
 #testing title
 st.title("*** ***TESTING PAGE*** ***")
+
+
 # --------------------------------     Copy/Paste code below this line     --------------------------------
+
+
 # Function to perform inference on uploaded image
 def rubbish_detector(image_file):
     with st.spinner('Please wait for image classification . . . .'):
@@ -34,36 +38,37 @@ def loadlocationdata():
     suburbs = [suburb.title() for suburb in suburbs]
     suburbs.sort()
     return suburbs
-def geolocate():
+
+
+def geolocate(country, state, city, road, number):
+    try:
     # Get location info using geopy
-    geolocator = Nominatim(user_agent="UTS_APP")
-    if selected_street is not None and selected_street != "":
-        if selected_number is not None and selected_number != "":
+        geolocator = Nominatim(user_agent="UTS_APP")
+
+        #check to see if locate_me function is being used
+        if 'locate_me' not in session_state:
+            state = "NSW"
+            country = "Australia"
+
+        if selected_number:
             geo_location = geolocator.geocode(selected_number +" "+selected_street +" "+ selected_suburb+", "+ state +", "+country,addressdetails=True)
             coordinates = (geo_location.latitude, geo_location.longitude)
             map = folium.Map(location=coordinates, zoom_start=17)
             folium.Marker([geo_location.latitude, geo_location.longitude]).add_to(map)
-        else:
-            geo_location = geolocator.geocode(selected_street +" "+ selected_suburb+", "+ state +", "+country,addressdetails=True)
-            coordinates = (geo_location.latitude, geo_location.longitude)
-            map = folium.Map(location=coordinates, zoom_start=17)
-            folium.Marker([geo_location.latitude, geo_location.longitude]).add_to(map)
-    else:
-        geo_location = geolocator.geocode(selected_suburb+", "+ state +", "+country,addressdetails=True)
-        coordinates = (geo_location.latitude, geo_location.longitude)
-        map = folium.Map(location=coordinates, zoom_start=14)
-    
-    # Display location address and coordinates
-    address = geo_location.address
-    if "house_number" not in geo_location.raw['address']:
-        address = selected_number + ", " + address
-        st.warning('Unable to find exact location on map', icon="⚠️")
-    st.write("Full Address:", address)
-    
-    #used for testing
-    #st.write(geo_location.raw['address'])
-    # Display map
-    return st_folium(map, height=400)
+
+        # Display location address and coordinates
+        address = geo_location.address
+        if "house_number" not in geo_location.raw['address']:
+            address = selected_number + ", " + address
+            st.info('Unable to find exact location on map. Your address has been saved.', icon="⚠️")
+        st.write("Full Address:", address)
+            
+            # Display map
+        return st_folium(map, height=400)
+    except Exception as e:
+        st.error(f"Map currently unavailable. Please double check the address. \n\nError Message: {e}")
+
+# Geolocation function
 def locate_me():
     latitude = loc['coords']['latitude']
     longitude = loc['coords']['longitude']
@@ -71,72 +76,81 @@ def locate_me():
     geolocator = Nominatim(user_agent="UTS_APP")
     location = geolocator.reverse(coordinates)
     address_raw = location.raw['address']
-    
-    #used for testing
-    #st.write(location.raw)
-    country = address_raw['country']
-    state = address_raw['state']
-    city = address_raw['city']
-    suburb = address_raw['city']
-    road = address_raw['road']
-    
-    return country, state, city, suburb, road
-# --------------------------------     Streamlit app     --------------------------------
+
+    country = address_raw.get('country')
+    state = address_raw.get('state')
+    road = address_raw.get('road')
+
+    # Define a list of potential fields for the city or suburb field
+    city_field = ['city', 'suburb', 'town', 'village']
+
+    # Iterate over the potential field names and retrieve the value if it exists
+    city = None
+    for key in city_field:
+        if key in address_raw:
+            city = address_raw[key]
+            break
+    return country, state, city, road
+
+# ----------------------------------------------------------------     Streamlit app     ----------------------------------------------------------------
 st.title("Curbside rubbish reporting app")
-#Run the geolocation
+#Run the geolocation engine
 loc = get_geolocation()
-#photo subheader
+
+#Photo subheader
 st.subheader("Please take a photo or upload an image")
+
 # Define a SessionState object
 session_state = st.session_state
 if 'image uploaded' not in session_state:
     session_state['image uploaded'] = None
-# Allow user to upload an image
+
+# Allow user to upload an image type=["jpg", "jpeg"]
 uploaded_image = st.file_uploader("Choose an image...", type=["jpg", "jpeg"])
 if uploaded_image is not None:
     # Display the uploaded image
     image = Image.open(uploaded_image)
     image = ImageOps.exif_transpose(image)
-    st.image(image, width=250, caption="Uploaded Image")
-    
-# Define a SessionState object
-session_state = st.session_state
+
 # Perform inference if an image is uploaded and the function has not been run yet or if a new image is uploaded
-if uploaded_image is not None and session_state['image uploaded'] !=  uploaded_image.name:
+if uploaded_image is not None and session_state['image uploaded'] !=  uploaded_image.name + str(uploaded_image.size):
     # Run the rubbish_detector function
     detected_object, confidence = rubbish_detector(image)
     session_state['detected_object'] = detected_object
     session_state['confidence'] = confidence
-    session_state['image uploaded'] = uploaded_image.name
+    session_state['image uploaded'] = uploaded_image.name + str(uploaded_image.size)
+
 # Load location data
 suburbs = loadlocationdata()
+
 # Allow user to select their location
-#if 'detected_object' in session_state:
 with st.container(border=True):
     st.subheader("Please enter the rubbish location ")
-    
+
     if st.button(":round_pushpin: Locate Me "):
         session_state['locate_me'] = True
+
     if 'locate_me' in session_state:
-        if session_state['locate_me'] == True:
-            try:
-                country, state, city, suburb, road = locate_me()
-                
-                suburbs.insert(0, suburb)
-                selected_suburb = st.selectbox("Suburb",suburbs, index=0, placeholder="Select a Suburb . . .",)
-                col1, col2 = st.columns(2)
-                selected_street = col1.text_input("Street Name", value=road,placeholder="Enter a Street Name . . .   e.g. Smith Street")
-                selected_number = col2.text_input("Street Number",placeholder="Enter your street number")
-            except:
-                st.warning('Geolocation service currently unavailable', icon="⚠️")
+        try:
+            country, state, city, road = locate_me()
+            suburbs.insert(0, city)
+        except:
+            st.warning('Geolocation service currently unavailable', icon="⚠️")
     else:
-        selected_suburb = st.selectbox("Suburb",suburbs, index=None, placeholder="Select a Suburb . . .",)
-        col1, col2 = st.columns(2)
-        selected_street = col1.text_input("Street Name", placeholder="Enter a Street Name . . .   e.g. Smith Street")
-        selected_number = col2.text_input("Street Number",placeholder="Enter your street number")
-    
-    if selected_street is not None and selected_street != "":
-        geolocate()
+        country = "Australia"
+        state = "NSW"
+
+    selected_suburb = st.selectbox("Suburb", suburbs, index=0 if 'locate_me' in session_state else None, placeholder="Select a Suburb . . .",)
+    col1, col2 = st.columns(2)
+    selected_street = col1.text_input("Street Name", value=road if 'locate_me' in session_state else "", placeholder="Enter a Street Name . . .   e.g. Smith Street")
+    selected_number = col2.text_input("Street Number", placeholder="Enter your street number")
+
+    if selected_suburb and (not selected_street or not selected_number):
+        st.warning("Please enter a value in every field.")
+
+    if selected_suburb and selected_street and selected_number:
+        geolocate(country, state, selected_suburb, selected_street, selected_number)
+
 # Display inference results if available
 if 'detected_object' in session_state:
     with st.container(border=True):
@@ -153,27 +167,26 @@ if 'detected_object' in session_state:
 # --------------------------------     Streamlit app - end     --------------------------------
 # ------------------------------------------------------------------------------------------------   New feature testing
 
-if st.button("TEST ME"):
-    latitude = loc['coords']['latitude']
-    longitude = loc['coords']['longitude']
-    coordinates = (latitude, longitude)
+with st.container(border=True):
+    st.subheader("Geolocation troubleshooting")
 
-    geolocator = Nominatim(user_agent="UTS_APP")
-    location = geolocator.reverse(coordinates)
-    address_raw = location.raw['address']
-    
-    #used for testing
-    st.write(location.raw)
+    if st.button("Geolocate Me"):
+        latitude = loc['coords']['latitude']
+        longitude = loc['coords']['longitude']
+        coordinates = (latitude, longitude)
+
+        geolocator = Nominatim(user_agent="UTS_APP")
+        location = geolocator.reverse(coordinates)
+        address_raw = location.raw['address']
+        
+        #used for testing
+        st.write(address_raw)
 
 #Testing only
-#with st.container(border=True):
-#    st.subheader("Backend Code information")
-#    session_state
+with st.container(border=True):
+    st.subheader("Backend Code information")
+    session_state
 
 
 
 # ------------------------------------------------------------------------------------------------   New feature testing
-
-with st.container(border=True):
-    st.subheader("Backend Code information")
-    session_state
