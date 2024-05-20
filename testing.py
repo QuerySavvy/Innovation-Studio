059c1247e1c2b8b2b6973d00e295ccb2ffacb4fd
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-20 09:56:36 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -13,6 +13,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 import time
 
+
 # initialize the client
 CLIENT = InferenceHTTPClient(
     api_url="https://detect.roboflow.com",
