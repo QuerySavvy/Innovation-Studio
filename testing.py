302904bdb0f50e949cff42aafc04c85597d9b9d8
QuerySavvy <145577861+QuerySavvy@users.noreply.github.com>
2024-05-22 22:50:14 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -198,6 +198,7 @@ def create_user(username, password, users):
         session_state['user_name'] = username
         session_state['user_row_number'] = next_row
         session_state['user_points'] = 0
+        time.sleep(1)
         st.rerun()
 
     else:
